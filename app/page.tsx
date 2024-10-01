8915f8bb916f7b1ff9f77342b9bc371afd1e2891
Ansh Gupta <hig6063@gmail.com>
2024-10-01 13:49:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="max-w-2xl mx-auto p-4 md:p-8">
         <h1 className="text-4xl font-bold mb-8">Hey, I'm Ansh</h1>
         <section className="mb-8">
-          <h2 className="inline-block bg-secondary px-2 py-1 text-sm mb-2">about</h2>
+          <h2 className="inline-block bg-secondary px-2 py-1 text-sm mb-2">about me</h2>
           <p>Hey I'm Ansh and I am a student and a <span >
           3 ⭐ Coder </span> 
           with a strong foundation in programming languages like C/C++,Java and Python and various frameworks like Pytorch.</p>
