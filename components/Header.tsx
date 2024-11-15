bca784c7388064777c2cbe04947dc40d381ff556
Ansh Gupta <hig6063@gmail.com>
2024-11-16 00:15:52 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,9 +13,9 @@ export function Header() {
       <nav className="max-w-2xl mx-auto px-4 flex justify-between items-center">
         <ul className="flex space-x-4">
           <li><Link href="/" className={`hover:underline ${pathname === '/' ? 'font-bold' : ''}`}>home</Link></li>
-          <li><Link href="/skills" className={`hover:underline ${pathname === '/skills' ? 'font-bold' : ''}`}>my skills</Link></li>
-          <li><Link href="/projects" className={`hover:underline ${pathname === '/projects' ? 'font-bold' : ''}`}>my projects</Link></li>
-          <li><Link href="/blogs" className={`hover:underline ${pathname === '/blogs' ? 'font-bold' : ''}`}>my blogs</Link></li>
+          <li><Link href="/skills" className={`hover:underline ${pathname === '/skills' ? 'font-bold' : ''}`}>skills</Link></li>
+          <li><Link href="/projects" className={`hover:underline ${pathname === '/projects' ? 'font-bold' : ''}`}>projects</Link></li>
+          <li><Link href="/blogs" className={`hover:underline ${pathname === '/blogs' ? 'font-bold' : ''}`}>blogs</Link></li>
         </ul>
         <button
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
