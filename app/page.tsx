c9702f181b3f81d878038af0104d095a40c65da1
Ansh Gupta <hig6063@gmail.com>
2025-02-24 13:09:10 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <h2 className="inline-block bg-secondary px-2 py-1 text-sm mb-2">who am I ?</h2>
           <p>
             A passionate student and a 3-star coder with a solid foundation in programming 
-            languages such as C/C++, Java, and Python. I have experience with various frameworks, including PyTorch. 
+            languages such as C/C++, Java, Python, Golang, Rust and Elixir. I have experience with various frameworks, including PyTorch. 
             <br>  </br> Recently, my startup idea was selected for the prestigious OnDeck Founders Fellowship program in San Francisco,
             as well as The Residency program backed by Sam Altman in Bengaluru. 
           </p>
