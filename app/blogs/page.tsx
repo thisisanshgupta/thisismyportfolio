0c2a7939fec7fd0f589ece5b3b880406427431b0
Ansh Gupta <hig6063@gmail.com>
2025-06-06 12:15:32 +0530

    Update page.tsx

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -8,6 +8,8 @@ export default function Blogs() {
       <main className="max-w-2xl mx-auto p-4 md:p-8">
         <h1 className="text-3xl font-bold mb-8">Blogs</h1>
         <ul className="list-disc list-inside space-y-2">
+          <li><Link href="https://dev.to/thisisanshgupta/what-the-hell-is-this-biological-computing-4fm2" className="underline">What the hell is this Biological Computing? </Link></li>      
+          <li><Link href="https://dev.to/thisisanshgupta/how-qubits-are-physically-implemented-5bd9" className="underline">How Qubits are physically implemented? </Link></li>
           <li><Link href="https://dev.to/thisisanshgupta/building-a-meta-search-engine-in-python-a-step-by-step-guide-1jb8" className="underline">Building a Meta Search Engine in Python: A Step-by-Step Guide </Link></li>
         </ul>
       </main>
